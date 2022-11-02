baa83b13bac9cbbee337e866e7459e0adc11b115
Leigh-Anne Niehaus <leigh-anneniehaus@Leigh-Annes-MacBook-Pro-2.local>
2022-11-01 21:00:13 -0700

    first commit

---
+++ letandconst.js
@@ -0,0 +1,14 @@
+const PI = 3.14;
+PI = 42;
+
+// What is the difference between var and let?
+// Var can be redeclared, and has a fucntion scope, while let cannot be redeclared and has blockscope. You can access a hoisted variable with Var
+
+// What is the difference between var and const?
+// Var can be both reassigned and redeclared, and has function scope, const cannot be reassigned or redeclared, and has block scope. You can access a hoisted variable with Var
+
+// What is the difference between let and const?
+// Let can be reassigned and const cannot be reassigned
+
+// What is hoisting?
+// Where the var declarations are run first, they are lifted to the top of the scope they are declared in. 
